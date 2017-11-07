49dbdec77790a20790889d29b713d48e53e4ff5b
Rene Christen <accounts@renechristen.net>
2017-11-07 20:53:17 +1100

    fix scrollbar include

--- src/SceneScrollViewWithScrollbar.h
+++ src/SceneScrollViewWithScrollbar.h
@@ -12,7 +12,7 @@
 #pragma once
 
 #include "SceneScrollView.h"
-#include "PopupSelectScrollbar.h"
+#include "SceneScrollbar.h"
 
 namespace coc {
 
@@ -29,11 +29,11 @@ public:
     static SceneScrollViewWithScrollbarRef create(const glm::ivec2 & size);
     static SceneScrollViewWithScrollbarRef create(int width, int height);
 
-    void setScrollbar(const PopupSelectScrollbarRef & value) { scrollbar = value; }
+    void setScrollbar(const SceneScrollbarRef & value) { scrollbar = value; }
 
     void update() override;
 
-    PopupSelectScrollbarRef scrollbar;
+    SceneScrollbarRef scrollbar;
 };
 
 };
